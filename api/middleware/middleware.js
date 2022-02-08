99a1eb83ce15bc5bc23a189e4903458205a32ad5
Jessweb87 <jprosper87@gmail.com>
2022-02-08 15:48:04 -0500

    completed

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,76 @@
+const User = require('../users/users-model')
+
+// - `logger()`
+// - `logger` logs to the console the following information about each request: request method, request url, and a timestamp
+// - this middleware runs on every request made to the API
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+    console.log(`[${timestamp}] ${method} to ${url}`)
+    next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+// - `validateUserId()`
+// - this middleware will be used for all user endpoints that include an `id` parameter in the url (ex: `/api/users/:id` and it should check the database to make sure there is a user with that id.
+// - if the `id` parameter is valid, store the user object as `req.user` and allow the request to continue
+// - if the `id` parameter does not match any user id in the database, respond with status `404` and `{ message: "user not found" }`
+
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: "user not found",
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(404).json({
+      message: 'user not found',
+    })
+  }
 }
+
+// - `validateUser()`
+//   - `validateUser` validates the `body` on a request to create or update a user
+//   - if the request `body` lacks the required `name` field, respond with status `400` and `{ message: "missing required name field" }`
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: "missing required name field",
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
+// - `validatePost()`
+//   - `validatePost` validates the `body` on a request to create a new post
+//   - if the request `body` lacks the required `text` field, respond with status `400` and `{ message: "missing required text field" }`
+
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field",
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+  logger,
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,135 @@
 const express = require('express');
+// Bring in middleware functions
+const { 
+  validateUserId, 
+  validateUser, 
+  validatePost 
+} = require('../middleware/middleware')
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
+const User = require('./users-model')
+const Post = require('../posts/posts-model');
+
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+// RETURN AN ARRAY WITH ALL THE USERS
+//   [GET] /api/users                                                                                    
+// √ [1] can get the correct number of users (54 ms)                                                 
+// √ [2] can get all the correct users (29 ms) 
+router.get('/', (req, res, next) => {
+   User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+// RETURN THE USER OBJECT
+// this needs a middleware to verify user id
+// [GET] /api/users/:id                                                                                
+// √ [3] can get the correct user (30 ms)                                                            
+// √ [4] responds with a 404 if id does not exist (28 ms)                                            
+// √ [5] responds with the correct error message if id does not exist (28 ms)
+router.get('/:id', validateUserId, (req, res) => {
+  res.json(req.user)
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+// RETURN THE NEWLY CREATED USER OBJECT
+// this needs a middleware to check that the request body is valid
+//    [POST] /api/users                                                                                   
+// √ [6] creates a new user in the database (48 ms)                                                  
+// √ [7] responds with the newly created user (36 ms)                                                
+// √ [8] responds with a 400 if missing name (25 ms)                                                 
+// √ [9] responds with the correct error message if missing name (27 ms)
+router.post('/', validateUser, (req, res, next) => {
+  User.insert({ name: req.name})
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+// RETURN THE FRESHLY UPDATED USER OBJECT
+// this needs a middleware to verify user id
+// and another middleware to check that the request body is valid
+// [PUT] /api/users/:id                                                                                
+// √ [10] writes the updates in the database (36 ms)                                                 
+// √ [11] responds with the newly updated user (35 ms)
+// √ [12] responds with a 404 if user id does not exist (30 ms)                                      
+// √ [13] responds with a 400 if missing name (32 ms)                                                
+// √ [14] responds with the correct error message if missing name (29 ms)
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+    .then(() => {
+      return User.getById(req.params.id)
+    })
+    .then(user => {
+      res.json(user)
+    })
+    .catch(next)
 });
 
-router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+// RETURN THE FRESHLY DELETED USER OBJECT
+// this needs a middleware to verify user 
+// [DELETE] /api/users/:id                                                                             
+// √ [15] deletes the user from the database (34 ms)                                                 
+// √ [16] responds with the newly deleted user (33 ms)
+// √ [17] responds with a 404 if user id does not exist (30 ms)
+router.delete('/:id', validateUserId, async (req, res, next) => {
+  try {
+    await User.remove(req.params.id)
+    res.json(req.user)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+// RETURN THE ARRAY OF USER POSTS
+// this needs a middleware to verify user id
+// [GET] /api/users/:id/posts                                                                          
+// √ [18] gets the correct number of user posts (33 ms)                                              
+// √ [19] responds with a 404 if user id does not exist (33 ms)  
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
+  try {
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+// RETURN THE NEWLY CREATED USER POST
+// this needs a middleware to verify user id
+// and another middleware to check that the request body is valid
+// [POST] /api/users/:id/posts                                                                         
+// √ [20] creates a new user post in the database (52 ms)                                            
+// √ [21] responds with the newly created user post (36 ms)                                          
+// √ [22] responds with a 404 if user id does not exist (30 ms)                                      
+// √ [23] responds with a 400 if missing text (30 ms)                                                
+// √ [24] responds with the correct error message if missing text (29 ms)
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
+    })
+    res.status(201).json(result)
+  } catch (err) {
+    next(err)
+  }
 });
+
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: 'something went wrong',
+    message: err.message,
+    stack: err.stack,
+  })
+})
 
 // do not forget to export the router
+
+module.exports = router
